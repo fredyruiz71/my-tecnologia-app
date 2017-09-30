6877f19ecae05b60fad3de1411f5834771a98f47
Fredy Ruiz <fredyruiz71@hotmail.com>
2017-09-30 14:50:47 -0500

    segundo push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProviderModule } from './provider/provider.module';
 import {OrdersModule} from './orders/orders.module';
 import { MenuComponent } from './menu/menu.component';
 import { SelectComponent } from './select/select.component';
+
+import {IndexComponent} from './provider/index/index.component';
+import {PcComponent} from './orders/pc/pc.component';
+
+
+const appRoutes: Routes = [
+  { path:"provider/index", component:IndexComponent},
+  { path:"pc.orders",         component: PcComponent },
+
+  {},
+
+];
 
 @NgModule({
   declarations: [
@@ -19,6 +32,7 @@ import { SelectComponent } from './select/select.component';
     BrowserModule,
     ProviderModule,
     OrdersModule,
+    RouterModule,
     
   ],
   providers: [],
